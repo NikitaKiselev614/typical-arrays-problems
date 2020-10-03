197e851103ffcb4f489ffa556b1cb83c17086d4e
NikitaKiselev614 <70860678+NikitaKiselev614@users.noreply.github.com>
2020-10-03 20:11:53 +0300

    init: typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (array !== undefined && array !== null && array.length > 0){
+    array.sort((a,b)=> a-b);
+    let minValue = array[0];
+    return minValue;
+  }
+  else
+  {
+    return 0; 
+  }
 }
 
-exports.max = function max (array) {
+exports.max = function max(array) {
+  if (array !== undefined && array !== null && array.length > 0) {
+  array.sort((a, b) => a - b);
+  let maxim = array[array.length-1];
+  return maxim;
+  }
+  else {
   return 0;
+  }
 }
 
-exports.avg = function avg (array) {
+exports.avg = function avg(array) {
+  if (array !== undefined && array !== null && array.length > 0) {
+  let sum = 0;
+  let srednee = 0;
+for (let i = 0; i < array.length; i++)
+ {
+  sum = sum + array[i];
+  srednee = sum / array.length;
+}
+  return srednee;
+}
+  else {
   return 0;
+}
 }
